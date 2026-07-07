14afcdf94cee66a773c838126e85acca37a21a9b
Luis Badilla <luisbadilla55@hotmail.com>
2026-07-06 22:52:15 -0600

    Cambio en estructura

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Layout from './layout.jsx'
+import Layout from './Layout.jsx'
 import RoadMap from './RoadMap.jsx'
 
 createRoot(document.getElementById('root')).render(
